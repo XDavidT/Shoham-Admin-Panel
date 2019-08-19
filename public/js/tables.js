d2cfa04883c94404f8cf3efe7c9832a6a0c3ae88
XDavidT <XDavidT@gmail.com>
2019-08-19 20:44:36 +0300

    Improved GUI in Logs page, Start testing in logs tables using javascript getting json format

---
+++ public/js/tables.js
@@ -0,0 +1,28 @@
+const arr =[
+    ['my id','is time','type','soruce','category','pc-david','1.1.1.1','xdavidt','windows'],
+
+    ['my id2','is tim2e','typ2e','soruce2','catego2ry','pc-d2avid','21.1.1.1','dsg','linux']
+]
+
+/*This java script page would get json files from MongoDB
+User will chose filters, and by those, we can filter specific data to show ! */
+
+const myTable = document.querySelector('tbody')
+function fillTable(){
+    while(myTable.firstChild){
+        myTable.removeChild(myTable.firstChild)
+    }
+
+    arr.forEach((item) => {
+        const tr = document.createElement('tr')
+
+        item.forEach((cell) =>{
+            const td = document.createElement('td')
+            td.textContent = cell
+            tr.appendChild(td)
+        })
+        myTable.appendChild(tr)
+    })
+    console.log("OK")
+}
+fillTable()
