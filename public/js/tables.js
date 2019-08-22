e87549234dd1ae80b6177fa8a870ba475b393226
XDavidT <XDavidT@gmail.com>
2019-08-22 18:28:22 +0300

    Data Table working as magic !!

--- public/js/tables.js
+++ public/js/tables.js
@@ -1,49 +1,21 @@
 $.getJSON('/logs/data2table',function(data){
-    fillTable(data)
+    $(document).ready(function() {
+        $('#dataTable').DataTable({
+            data:data,
+            columns: [
+                {data: 'logid'},
+                {data: 'time'},
+                {data: 'type'},
+                {data: 'src'},
+                {data: 'cat'},
+                {data: 'hostname'},
+                {data: 'ip_add'},
+                {data: 'username'},
+                {data: 'os'}
+            ]
+        });
+      });
 })
-
-
-function fillTable(logsData){
-    const myTable = document.querySelector('tbody')
-    // Clear the table
-    while(myTable.firstChild){
-        myTable.removeChild(myTable.firstChild)
-    }
-    logsData.forEach(function (_log) {
-        const tr = document.createElement('tr')
-
-
-        const td1 = document.createElement('td')
-        td1.textContent = _log.logid
-        tr.appendChild(td1)
-        const td2 = document.createElement('td')
-        td2.textContent = _log.time
-        tr.appendChild(td2)
-        const td3 = document.createElement('td')
-        td3.textContent = _log.type
-        tr.appendChild(td3)
-        const td4 = document.createElement('td')
-        td4.textContent = _log.src
-        tr.appendChild(td4)
-        const td5 = document.createElement('td')
-        td5.textContent = _log.cat
-        tr.appendChild(td5)
-        const td6 = document.createElement('td')
-        td6.textContent = _log.hostname
-        tr.appendChild(td6)
-        const td7 = document.createElement('td')
-        td7.textContent = _log.ip_add
-        tr.appendChild(td7)
-        const td8 = document.createElement('td')
-        td8.textContent = _log.username
-        tr.appendChild(td8)
-        const td9 = document.createElement('td')
-        td9.textContent = _log.os
-        tr.appendChild(td9)
-
-        myTable.appendChild(tr)
-    })
-}
 
 
 //TODO: Understand how datatable is generate (select entries etc..)
--- src/utilities/mongoHandler.js
+++ src/utilities/mongoHandler.js
@@ -5,13 +5,6 @@ const connectionURL = 'mongodb://192.168.0.128:27017'
 const databaseName = 'clientManager'
 const collectionName = 'clientLog'
 
-// mongoClient.connect(connectionURL,{useNewUrlParser: true},(error, client) => {
-//     if(error){
-//         return console.log('Unable to connect database')
-//     }
-//     const clientMgrDB = client.db(databaseName)
-// })
-
 // Using in callback style
 const getLogsFromDB = (myfilter,callback) => {
     mongoClient.connect(connectionURL,{useNewUrlParser: true},(error, client) => {
@@ -19,7 +12,7 @@ const getLogsFromDB = (myfilter,callback) => {
             callback(error,undefined)
         }
         const clientLogs = client.db(databaseName).collection(collectionName)
-        const result = clientLogs.find(myfilter,{limit:25}).toArray((error,logList) => {
+        const result = clientLogs.find(myfilter).toArray((error,logList) => {
             callback(undefined,logList)
         })
         //when finish - Close the connection!!
