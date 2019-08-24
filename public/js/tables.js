07a834d30385439e155210232cdb06127cd6d260
XDavidT <XDavidT@gmail.com>
2019-08-24 19:41:34 +0300

    New feature: Filter logs in /log page.

--- public/js/tables.js
+++ public/js/tables.js
@@ -1,6 +1,24 @@
 $.getJSON('/logs/data2table',function(data){
     $(document).ready(function() {
-        $('#dataTable').DataTable({
+
+        $('#dataTable thead tr').clone(true).appendTo( '#dataTable thead' );
+        $('#dataTable thead tr:eq(1) th').each( function (i) {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+     
+            $( 'input', this ).on( 'keyup change', function () {
+                if ( table.column(i).search() !== this.value ) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+
+        var table = $('#dataTable').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
             data:data,
             columns: [
                 {data: 'logid'},
@@ -18,4 +36,3 @@ $.getJSON('/logs/data2table',function(data){
 })
 
 
-//TODO: Understand how datatable is generate (select entries etc..)
