19e81fe3f5e38c3d4d79bdbe4d98b32cb7fa735f
David <xdavidt@gmail.com>
2019-09-11 07:30:19 +0300

    Added loading

--- public/js/tables.js
+++ public/js/tables.js
@@ -1,6 +1,24 @@
 $.getJSON('/logs/data2table',function(data){
     $(document).ready(function() {
-        $('#dataTableLogs').DataTable({
+
+        $('#dataTableLogs thead tr').clone(true).appendTo( '#dataTableLogs thead' );
+        $('#dataTableLogs thead tr:eq(1) th').each( function (i) {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+     
+            $( 'input', this ).on( 'keyup change', function () {
+                if ( table.column(i).search() !== this.value ) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+
+        var table = $('#dataTableLogs').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
             data:data,
             columns: [
                 {data: 'logid'},
@@ -10,11 +28,16 @@ $.getJSON('/logs/data2table',function(data){
                 {data: 'cat'},
                 {data: 'hostname'},
                 {data: 'ip_add'},
+                {data: 'mac_add'},
                 {data: 'username'},
                 {data: 'os'}
             ]
         });
       });
-})
+}).done(function() {
+    $('.loading-section').hide();   //Hide Loading GIF when done to load the logs
+  }).fail(function() {
+    console.log( "error" );
+  })
 
 
