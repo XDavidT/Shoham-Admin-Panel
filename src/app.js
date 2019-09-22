4072c26fdad7c2a90812193f4f4e2dc59b15e069
ofekGamliel <ofekgaml@gmail.com>
2019-09-23 00:38:11 +0300

    Add Policy Page

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const mongo_handler = require('./utilities/Logs/handler/LogsHandler')
+const policy_handler = require('./utilities/Policy/handler/policyHandler')
 const app = express()
 require('./utilities/Users/handler/UsersHandler')
 
@@ -20,17 +21,86 @@ app.use(express.static(public_dir))
 app.use(express.json())
 //app.use(userRouter)
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.get('/logs',(req, res) =>{
     res.render('logs',{
         
     })
 })
 
+
 app.get('/api/logs/data2table',(req, res) => {
     mongo_handler.getLogsFromDB(req.query,(error,result) =>{
         if(error){
             res.send(error)
         } else {
+            res.jsonp(result)
+        }
+    })
+})
+
+app.get('/policy',(req, res) =>{
+    res.render('policy',{
+    })
+})
+
+app.post('/api/policy/postRules', (req, res) => {
+
+    policy_handler.postRulesToDB(req.body,(error,result) =>{
+        if(error){
+            res.send(error)
+        } else {
+            res.redirect('/policy')
+        }
+    })
+
+})
+
+
+
+app.get('/api/policy/data2table',(req, res) => {
+    policy_handler.getRulessFromDB(req.query,(error,result) =>{
+        if(error){
+            res.send(error)
+        } else {
+            res.jsonp(result)
+        }
+    })
+})
+
+app.post('/api/policy/postEvents', (req, res) => {
+    console.log(req.body)
+    policy_handler.postEventsToDB(req.body,(error,result) =>{
+        if(error){
+            res.send(error)
+        } else {
+            console.log("Sadsadasdasdasdas")
+            res.redirect('/policy')
+        }
+    })
+
+})
+
+app.post('/policy/category_select', (req, res) => {
+    policy_handler.postCategoryToDB(req.body,(error,result) =>{
+        if(error){
+            res.send(error)
+        } else {
+            res.redirect('/policy')
+        }
+    })
+
+})
+
+app.get('/api/policy/eventData2table',(req, res) => {
+    policy_handler.getEventsFromDB(req.query,(error,result) =>{
+        if(error){
+            res.send(error)
+        } else {
+            console.log(result)
             res.jsonp(result)
         }
     })
