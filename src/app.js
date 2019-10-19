377ea4b427525ab0b6b5fd81093c62b0e8064e0b
sahari6767 <sahari6767@gmail.com>
2019-10-19 12:41:13 +0300

    login move to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+//login necessary consts
+const User = require('../src/utilities/models/user_model')
+const logout = require('../src/Routers/logout')
+const Authenticate = require('../src/Routers/Authentication')
+
+
 //Parsers
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
@@ -49,13 +55,37 @@ app.use(session({
 app.use(flash())
 
 app.use(site_Router)
-app.use('/api/setting',gen_Router)
-app.use(user_Router)
+app.use('/api/setting', gen_Router)
+app.use('/api/users', user_Router)
 app.use('/api/logs', logs_Router)
 app.use('/api/policy', policy_Router)
 
 
 
+
+//Login User 
+app.post('/login', async (req, res) => {
+    try{
+    const user = await User.findByCredentials(req.body.email , req.body.password)
+    const token = await user.generateAuthToken()
+    res.cookie('token',token,{'maxAge': 3600000, httpOnly: true}) // sending cookie with expire time of 1 Hour.
+    res.redirect(302 , 'http://localhost:3000')
+    console.log("Login Succes")
+    } catch(error){
+        console.log("Login Error")
+        res.status(400).send('Login error')
+    }
+})
+
+//Logout User
+app.post('/logout', Authenticate, logout, async (req, res) => {
+    console.log('here')
+    try {
+        await req.user.save()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 //Get resolver for site
 app.get('', (req, res) => {
--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -9,15 +9,12 @@ const user_router = new express.Router()
 
 
 //getting Authenticated User from DB
-user_router.get('/users/me', Authenticate, async (req , res) => {
+user_router.get('/me', Authenticate, async (req , res) => {
        res.send(req.user)
 })
 
-
-
-
-//getting Users from DB and be Authorized!
-user_router.get('/users/data2table', Authenticate , async (req, res) => {
+//getting Users from DB and be Authenticated!
+user_router.get('/data2table', Authenticate , async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
@@ -27,7 +24,7 @@ user_router.get('/users/data2table', Authenticate , async (req, res) => {
 })
 
 //getting one User from DB
-user_router.get('/users/:id', async (req, res) => {
+user_router.get('/:id', async (req, res) => {
     const _id = req.params.id
 
     try {
@@ -44,7 +41,7 @@ user_router.get('/users/:id', async (req, res) => {
 })
 
 //Updating User to DB
-user_router.patch('/users/:id', async (req, res) => {
+user_router.patch('/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -66,7 +63,7 @@ user_router.patch('/users/:id', async (req, res) => {
 })
 
 //Creating User to DB
-user_router.post('/users/add', Authorize, async (req, res) => {
+user_router.post('/add', Authorize, async (req, res) => {
     const user = new User(req.body)
 
     try {
@@ -78,31 +75,10 @@ user_router.post('/users/add', Authorize, async (req, res) => {
     }
 })
 
-//Login User 
-user_router.post('/users/login', async (req, res) => {
-    try{
-    const user = await User.findByCredentials(req.body.email , req.body.password)
-    const token = await user.generateAuthToken()
-    res.cookie('token',token,{'maxAge': 3600000, httpOnly: true}) // sending cookie with expire time of 1 Hour.
-    res.redirect(302 , 'http://localhost:3000')
-    console.log("Login Succes")
-    } catch(error){
-        console.log("Login Error")
-        res.status(400).send('Login error')
-    }
-})
 
-//Logout User
-user_router.post('/users/logout', Authenticate, logout, async (req, res) => {
-    try {
-        await req.user.save()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 //Logout All-Users
-user_router.post('/users/logoutAll', Authenticate, async (req, res) => {
+user_router.post('/logoutAll', Authenticate, async (req, res) => {
     try {
         req.user.tokens = []
         await req.user.save()
@@ -112,8 +88,10 @@ user_router.post('/users/logoutAll', Authenticate, async (req, res) => {
     }
 })
 
+
+
 //deleting User from DB
-user_router.delete('/users/delete', async (req, res) => {
+user_router.delete('/delete', async (req, res) => {
     try{
         const user = await User.findById(req.body)
         if(!user.root){
@@ -126,7 +104,7 @@ user_router.delete('/users/delete', async (req, res) => {
 })
 
 //deleting All documents from DB
-user_router.delete('/users' , async (req, res) => { 
+user_router.delete('delete_all' , async (req, res) => { 
     try{
     const user = await User.remove({}, function(err,removed) {
         res.status(200).send('Delete success')
