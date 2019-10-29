f2bf2b067570ef5a8f3cdf1f2aa048ff265a6e37
David <xdavidt@gmail.com>
2019-10-29 07:45:41 +0200

    Change users DB to Atlas

--- src/utilities/handler/UsersHandler.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-////CoonectTo-Users-DB////1
-mongoose.connect('mongodb://mongo.davidt.net:27018/SystemManagment',{
-  // mongoose.connect('mongodb://localhost:27017/SystemManagment',{
-    useNewUrlParser: true ,
-    useCreateIndex: true,
-    useUnifiedTopology: true },
-)
