db9f13cf51b7bb8d02d2fbea9cf402ee9b081411
David <xdavidt@gmail.com>
2019-11-06 13:53:48 +0200

    title been update index page by JS

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    $('#title').text('Siem Admin Dashboard')
+
     $('#LogCountHead').text('Logs in Database')
     $.get('/api/logs/count',function(result){
         $('#LogsCount').text(result)
