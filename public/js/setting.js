773d4a3e379bb9c6499d59c95c384a660f62ea59
XDavidT <XDavidT@gmail.com>
2019-11-11 20:03:10 +0200

    system name input fix bug

--- public/js/setting.js
+++ public/js/setting.js
@@ -13,7 +13,7 @@ $(document).ready( ()=> {
     req_filter['_id'] = "basic-setting"
     $.getJSON( '/api/gen/get-setting',req_filter)
     .done((jsonData)=> {
-        $("#SystemName").val(jsonData['system-name'])
+        $("#SystemNameInput").val(jsonData['system-name'])
         $("#TimeToLoad").val(jsonData['time_to_load'])
         $("#XTimeBack").val(jsonData['logs-from-X-hours'])
         $("#BasedOn").val(jsonData['local_based_on'])
@@ -26,7 +26,7 @@ $(document).ready( ()=> {
 $("#submit-setting").click(()=>{
     const jsonString = {}
     jsonString['_id'] = "basic-setting"
-    jsonString['system-name'] = $("input#SystemName").val()
+    jsonString['system-name'] = $("input#SystemNameInput").val()
     jsonString['time_to_load'] = $("input#TimeToLoad").val()
     jsonString['logs-from-X-hours'] = $("input#XTimeBack").val()
     jsonString['local_based_on'] = $("select#BasedOn").val()
